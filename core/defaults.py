e21e11fefa2c18ecd703841f062b385652f4cfa7
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-07-23 15:52:24 +0800

    added default settings file

---
+++ core/defaults.py
@@ -0,0 +1,18 @@
+DEFAULT_SETTINGS = [
+    {
+        "key": "site_title",
+        "name": "Site Title",
+        "value": "FlaskDash",
+        "type": "text",
+        "description": "The title of your site, displayed in the header.",
+        "editable": True
+    },
+    {
+        "key": "site_description",
+        "name": "Site Description",
+        "value": "A simple Flask dashboard application.",
+        "type": "textarea",
+        "description": "A brief description of your site for SEO purposes.",
+        "editable": True
+    }
+]
