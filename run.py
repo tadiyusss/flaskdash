029ada2e83534bc34cf98286be31d060aaf2668e
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-07-23 15:53:09 +0800

    added a check if app.db exists

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from core import create_app
+import os
 
-app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if 'app.db' in os.listdir('.') and os.path.isfile('app.db'):
+    print("Database not found. Please run setup.py to initialize the database.")
+
+    app = create_app()
+
+    if __name__ == "__main__":
+        app.run(debug=True)
