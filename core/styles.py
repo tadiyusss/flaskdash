1cb9c87ff26a33c220d65bb3f45affaae5f52715
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-07-23 15:54:55 +0800

    style for textarea

--- core/styles.py
+++ core/styles.py
@@ -2,3 +2,4 @@ class Style:
     text_input = "mt-1 w-full px-2 py-1 rounded text-sm border focus:outline-none focus:ring-1 focus:ring-blue-500/50 shadow-inner "
     button = "text-sm rounded px-2 py-1 rounded border shadow text-gray-700 hover:bg-gray-50 ease duration-200 cursor-pointer "
     button_full = "text-sm rounded py-2 rounded border shadow text-gray-700 hover:bg-gray-50 ease duration-200 cursor-pointer w-full " 
+    textarea = "mt-1 w-full px-2 py-1 rounded text-sm border focus:outline-none focus:ring-1 focus:ring-blue-500/50 shadow-inner h-32 resize-none"
