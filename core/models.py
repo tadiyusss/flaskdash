69778bc5ba10667946e5fe70376ed39f18954b54
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-07-24 15:39:16 +0800

    added uid on user, fixed settings relationship

--- core/models.py
+++ core/models.py
@@ -2,7 +2,9 @@ from .extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from sqlalchemy import Enum
+import uuid
 import enum
+
 
 user_roles = db.Table('user_roles',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
@@ -18,6 +20,7 @@ class User(db.Model, UserMixin):
     __table_args__ = (
         db.UniqueConstraint('username', name='uq_user_username'),
         db.UniqueConstraint('email', name='uq_user_email'),
+        db.UniqueConstraint('uid', name='uq_user_uid'),
     )
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
@@ -27,13 +30,13 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(128), nullable=False)
     profile_image = db.Column(db.String(200), default='default-avatar.jpg')
     role = db.Column(db.String(50), default='viewer')
+    uid = db.Column(db.String(36), default=lambda: str(uuid.uuid4()), unique=True)
 
     def set_password(self, raw_password):
         self.password = generate_password_hash(raw_password)
     
     def check_password(self, raw_password):
         return check_password_hash(self.password, raw_password)
-    
 
 class SettingsType(enum.Enum):
     text = 'text'
@@ -59,6 +62,28 @@ class Setting(db.Model):
 
     def __repr__(self):
         return f"<Setting {self.name} ({self.key})>"
+
+    @staticmethod
+    def create_or_update(name, key, value, type=SettingsType.text, description=None, editable=True):
+        setting = Setting.query.filter_by(key=key).first()
+        if setting:
+            setting.name = name
+            setting.value = value
+            setting.type = type
+            setting.description = description
+            setting.editable = editable
+        else:
+            setting = Setting(
+                name=name,
+                key=key,
+                value=value,
+                type=type,
+                description=description,
+                editable=editable
+            )
+            db.session.add(setting)
+        db.session.commit()
+        return setting
     
     def to_dict(self):
         return {
