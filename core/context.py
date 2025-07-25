fdcce62ccbae676af6e72953bd2ac69c681bf9ba
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-07-26 02:33:22 +0800

    added registry folder for modularity (will add more registry later)

--- core/context.py
+++ core/context.py
@@ -1,6 +1,6 @@
 from flask import g, current_app
 from core.models import Setting
-from core.utils.side_navigation import get_sidebar_items_for_user
+from core.utils.registry.side_navigation import get_sidebar_items_for_user
 from flask_login import current_user
 
 def init_context(app):
