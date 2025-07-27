72dd6474db4ffc3f0209afa72e5ba49dbb83d51a
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-07-27 23:43:15 +0800

    show the analytics

--- core/views/home.py
+++ core/views/home.py
@@ -1,8 +1,10 @@
 from flask import render_template
 from flask_login import login_required, current_user
+from core.utils.registry import analytics
 
 def generate_blueprint(core):
     @core.route('/home')
     @login_required
     def dashboard():
-        return render_template('dashboard/home.html', user=current_user)
+        analytics_items = analytics.get_analytics_items(current_user)
+        return render_template('dashboard/home.html', user=current_user, analytics_items=analytics_items)
