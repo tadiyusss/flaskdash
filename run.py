cb183891c58289db2f5d1c7ccd1cfdf71874f45f
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-07-27 19:27:46 +0800

    restructured

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from core import create_app
 import os
 
-
-if 'app.db' in os.listdir('.') and os.path.isfile('app.db'):
-    
-    app = create_app()
-
-    if __name__ == "__main__":
-        app.run(debug=True)
-else:
+if not 'app.db' in os.listdir('.') or not os.path.isfile('app.db'):
     print("Database not found. Please run setup.py to initialize the database.")
+    exit(1)
+
+if __name__ != "__main__":
+    exit(0)
+
+app = create_app()
+app.run(debug=True)
