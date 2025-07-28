024a139f0288d08cb119444bbf06af13531ed508
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-07-28 14:07:21 +0800

    check if extensions folder exists

--- core/initializations/extensions.py
+++ core/initializations/extensions.py
@@ -3,6 +3,10 @@ import importlib
 
 EXTENSIONS_LOCATION = os.path.join(os.path.dirname(__file__), "../../extensions/")
 
+# check if the extensions directory exists
+if not os.path.exists(EXTENSIONS_LOCATION):
+    os.makedirs(EXTENSIONS_LOCATION)
+
 def get_extension_metadata(extension_name):
     metadata_file = os.path.join(EXTENSIONS_LOCATION, extension_name, "metadata.py")
     if os.path.exists(metadata_file):
@@ -24,8 +28,8 @@ def is_valid_extension(extension_name):
 def register_extension(app, extension_name):
     metadata = get_extension_metadata(extension_name)
     bp = importlib.import_module(f"extensions.{extension_name}").bp
-    app.register_blueprint(bp)
-            
+    app.register_blueprint(bp, url_prefix=metadata.URL_PREFIX, static_folder=metadata.STATIC_FOLDER, template_folder=metadata.TEMPLATE_FOLDER)
+
 def register_all_extensions(app):
     for extension in list_extensions():
         register_extension(app, extension)
