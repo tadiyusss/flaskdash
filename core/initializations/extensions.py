54582e439e909fbc455866c70d6431fd0ac7ae80
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-07-29 14:26:58 +0800

    moved the other arguments to the extensions __init__.py

--- core/initializations/extensions.py
+++ core/initializations/extensions.py
@@ -3,9 +3,8 @@ import importlib
 from core.utils.registry.extensions import get_extension_metadata, list_extensions, register_extension
 
 def include_extension(app, extension_name):
-    metadata = get_extension_metadata(extension_name)
     bp = importlib.import_module(f"extensions.{extension_name}").bp
-    app.register_blueprint(bp, url_prefix=metadata.URL_PREFIX, static_folder=metadata.STATIC_FOLDER, template_folder=metadata.TEMPLATE_FOLDER)
+    app.register_blueprint(bp)
 
 def include_all_extensions(app):
     for extension in list_extensions():
