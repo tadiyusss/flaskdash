4d24a8295bbf86f9b678f6563fce1d1e1b1f35d2
Thaddeus Toledo <toledothaddeusf@gmail.com>
2026-07-13 18:35:29 +0800

    added a tailwind.config.js file for a standalone npm build

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",
+    "./**/*.js",
+    "./**/*.py",
+    "./extensions/**/*.html",
+    "./extensions/**/*.js",
+    "./extensions/**/*.py",
+  ],
+  theme: {},
+  plugins: [],
+}
