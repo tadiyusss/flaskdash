d0e0804080b5d5986041a51de745deab74f32fb8
Thaddeus Toledo <toledothaddeusf@gmail.com>
2026-08-21 14:48:07 +0800

    fixed migration env file patching

--- core/initializations/extensions.py
+++ core/initializations/extensions.py
@@ -16,6 +16,14 @@ def include_extension(app, extension_name):
     extension = importlib.import_module(f"extensions.{extension_name}").init_extension(app, db)
     app.register_blueprint(extension)
 
+def check_if_env_file_patched(extension_name):
+    env_file = Path(f"extensions/{extension_name}/migrations/env.py")
+
+    if not env_file.exists():
+        return False
+
+    content = env_file.read_text()
+    return f'version_table="alembic_version_{extension_name}"' in content
 
 def include_all_extensions(app):
     for extension in list_extensions():
@@ -29,8 +37,9 @@ def handle_extension_migrations(extension_name, app):
 
     with app.app_context():
         if migration_path.exists():
-            # check if patched
-            if not (migration_path / "env.py").exists():
+            # check if the "alembic_version_{extension_name}" exists in the env file
+            if not check_if_env_file_patched(extension_name):
+                logger.info(f"Patching env.py for extension: {extension_name}")
                 patch_env_file(extension_name)
 
 
